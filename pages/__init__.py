b9572b53f90e127c467275e75e75911d1d4335d3
SanjeevSinjali <tdkzbiru@gmail.com>
2024-03-03 16:12:51 +0545

    removed unnecessary files

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,7 +1,6 @@
 __all__ = [
     "admin_home",
     "admin_login",
-    "admin_register",
     "cus_home",
     "cus_login",
     "cus_register",
--- pages/staff_register.py
+++ pages/staff_register.py
@@ -1,5 +1,3 @@
-# register_page.py
-
 import tkinter as tk
 from tkinter import ttk,messagebox
 import re
